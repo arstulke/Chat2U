23bd53e76be5c7fb8f3b400eb309d1a3366f3b72
KarlAbert <karl.abert@btc-ag.com>
2016-11-15 10:41:14 +0100

    Structure Commit 3

---
+++ tests/de/chat2u/client/cucumber/feature/logout.feature
@@ -0,0 +1,6 @@
+#language:de
+Funktionalität: Abmelden
+	Szenario: vom Server abmelden
+		Gegeben sei der Teilnehmer "Carsten" in der Liste der Teilnehmer, die gerade Online sind.
+		Wenn "Carsten" sich abmeldet,
+		Dann sehe ich "Carsten" nicht mehr in der Liste der Teilnehmer, die gerade Online sind.
---
+++ tests/de/chat2u/client/unit/Tests.java
@@ -0,0 +1,19 @@
+package de.chat2u.client.unit;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Chat2U:
+ * * de.chat2u.client.unit:
+ * * * Created by KAABERT on 15.11.2016.
+ */
+public class Tests {
+
+    @Test
+    public void test(){
+        assertThat(0, is(0));
+    }
+}
